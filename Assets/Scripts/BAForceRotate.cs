5be871f7e78ca52e6f850b7f908561ac475cd08f
kaleidosgu <gjcaoyi@163.com>
2019-11-01 09:56:15 +0800

    整理BAForceRotate代码

--- Assets/Scripts/BAForceRotate.cs
+++ Assets/Scripts/BAForceRotate.cs
@@ -7,45 +7,32 @@ using BehaviorDesigner.Runtime.Tasks;
 [TaskCategory("Custom/Action")]
 class BAForceRotate : Action
 {
-    public SharedVector3 SharedVector3;
-    public SharedGameObject TargetShared;
-
-    public float DiffDistance;
-    public bool ProcessSucess;
+    public SharedVector3 ShareDestPos;
+    public SharedGameObject VirtualObject;
 
     private Rigidbody2D m_rigidBody;
     private AIAttributeComponent m_attr;
     public override TaskStatus OnUpdate()
     {
         Vector3 vecPosTarget;
-        if(TargetShared.Value != null)
+        if(VirtualObject.Value != null)
         {
-            vecPosTarget = TargetShared.Value.transform.position;
+            vecPosTarget = VirtualObject.Value.transform.position;
         }
         else
         {
-            vecPosTarget = SharedVector3.Value;
+            vecPosTarget = ShareDestPos.Value;
         }
-        if(ProcessSucess == true)
-        {
-            Vector2 vecCheck = transform.position - vecPosTarget;
-            float fSqrDistance = (vecCheck).sqrMagnitude;
-            if (fSqrDistance < DiffDistance)
-            {
-                return TaskStatus.Failure;
-            }
-        }
-        Debug.Log(string.Format("x[{0}],y[{1}]", vecPosTarget.x, vecPosTarget.y));
         Vector2 vecDirToTarget = vecPosTarget - transform.position;
 
         vecDirToTarget.Normalize();
 
         float valRotation = Vector3.Cross(transform.right, vecDirToTarget).z;
 
-        float fDistance = Vector2.Distance(transform.position, vecPosTarget);
+        float fDistanceTarget = Vector2.Distance(transform.position, vecPosTarget);
 
         float fMoveForce = m_attr.ForceValue;
-        if (fDistance >= m_attr.LimitDistance)
+        if (fDistanceTarget >= m_attr.LimitDistance)
         {
             fMoveForce = m_attr.PowerForceValue;
         }
